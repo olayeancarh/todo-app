d047c7087bd568341f86e624ae5f8f28150e94b6
olayeancarh <akejuolayinka016@gmail.com>
2025-08-28 00:19:20 +0100

    feat: enhance TodoList component with ngOnInit lifecycle and localStorage integration

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 interface Todo {
   id: number;
@@ -12,31 +12,35 @@ interface Todo {
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent {
-  todos: Todo[] = [
-    { id: 1, task: 'Learn Angular', completed: false },
-    { id: 2, task: 'Build To-Do App', completed: false }
-  ];
+export class TodoListComponent implements OnInit{
+  todos: Todo[] = [];
+
+  ngOnInit(): void {
+    this.todos = JSON.parse(localStorage.getItem('todos') || '[]');
+  }
 
   addTodo(task: string) {
-    const newTodo: Todo = {
-      id: this.todos.length + 1,
-      task,
-      completed: false
-    };
-
-    this.todos =  [...this.todos, newTodo];
+    const newTodo: Todo = { id: this.todos.length + 1, task, completed: false };
+    this.todos = [...this.todos, newTodo];
+    this.saveTodos();
   }
 
   toggleTodo(id: number) {
     this.todos = this.todos.map(todo => todo.id !== id ? todo : { ...todo, completed: !todo.completed });
+    this.saveTodos();
   }
 
   deleteTodo(id: number) {
     this.todos = this.todos.filter(todo => todo.id !== id);
+    this.saveTodos();
   }
 
-  markAllCompleted() {
+  markAllAsCompleted() {
     this.todos = this.todos.map(todo => ({ ...todo, completed: true }));
+    this.saveTodos();
+  }
+
+  private saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
