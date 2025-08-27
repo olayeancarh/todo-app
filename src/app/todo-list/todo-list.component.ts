5d1bc53b605ce5a47c81400903785788e07231ec
Olayinka Akeju <akejuolayinka016@gmail.com>
2025-08-27 23:07:12 +0100

    feat: add TodoList component with functionality to add, delete, and toggle tasks

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -13,5 +13,30 @@ interface Todo {
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-  
+  todos: Todo[] = [
+    { id: 1, task: 'Learn Angular', completed: false },
+    { id: 2, task: 'Build To-Do App', completed: false }
+  ];
+
+  addTodo(task: string) {
+    const newTodo: Todo = {
+      id: this.todos.length + 1,
+      task,
+      completed: false
+    };
+
+    this.todos =  [...this.todos, newTodo];
+  }
+
+  toggleTodo(id: number) {
+    this.todos = this.todos.map(todo => todo.id !== id ? todo : { ...todo, completed: !todo.completed });
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  markAllCompleted() {
+    this.todos = this.todos.map(todo => ({ ...todo, completed: true }));
+  }
 }
