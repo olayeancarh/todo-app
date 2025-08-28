ef08b26c11ef2bbe9e9afa780fc3b46b6ef2ab28
olayeancarh <akejuolayinka016@gmail.com>
2025-08-28 03:13:02 +0100

    test: add unit tests for TodoListComponent and configure Karma

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/todo-app"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["ChromiumHeadless"],
+    customLaunchers: {
+      ChromiumHeadless: {
+        base: "Chromium",
+        flags: [
+          "--no-sandbox",
+          "--disable-gpu",
+          "--headless",
+          "--remote-debugging-port=9222",
+        ],
+      },
+    },
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
+// Karma configuration file, see link for more information
--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoListComponent } from './todo-list.component';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
+
+  beforeEach(() => {
+    // Clear localStorage before each test
+    localStorage.clear();
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,5 +23,56 @@ describe('TodoListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a todo', () => {
+    component.addTodo('Test Task');
+    expect(component.todos.length).toBe(1);
+    expect(component.todos[0].task).toBe('Test Task');
+    expect(component.todos[0].completed).toBeFalse();
+  });
+
+  it('should not add an empty todo', () => {
+    component.addTodo('');
+    expect(component.todos.length).toBe(0);
+    component.addTodo('   ');
+    expect(component.todos.length).toBe(0);
+  });
+
+  it('should toggle a todo', () => {
+    component.addTodo('Toggle Task');
+    const id = component.todos[0].id;
+    component.toggleTodo(id);
+    expect(component.todos[0].completed).toBeTrue();
+    component.toggleTodo(id);
+    expect(component.todos[0].completed).toBeFalse();
+  });
+
+  it('should delete a todo', () => {
+    component.addTodo('Delete Task');
+    const id = component.todos[0].id;
+    component.deleteTodo(id);
+    expect(component.todos.length).toBe(0);
+  });
+
+  it('should mark all as completed', () => {
+    component.addTodo('Task 1');
+    component.addTodo('Task 2');
+    component.markAllAsCompleted();
+    expect(component.todos.every(todo => todo.completed)).toBeTrue();
+  });
+
+  it('should persist todos to localStorage', () => {
+    component.addTodo('Persist Task');
+    const stored = JSON.parse(localStorage.getItem('todos') || '[]');
+    expect(stored.length).toBe(1);
+    expect(stored[0].task).toBe('Persist Task');
+  });
+
+  it('should load todos from localStorage on init', () => {
+    localStorage.setItem('todos', JSON.stringify([{ id: 1, task: 'Loaded Task', completed: false }]));
+    component.ngOnInit();
+    expect(component.todos.length).toBe(1);
+    expect(component.todos[0].task).toBe('Loaded Task');
   });
 });
